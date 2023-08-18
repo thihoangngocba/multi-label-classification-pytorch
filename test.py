e7096dd1539800dbc233cbd978a2f3eb8390f833
HOÀNG NGỌC BÁ THI <115174809+thihoangngocba@users.noreply.github.com>
2023-08-18 14:47:52 +0700

    Update test.py

--- test.py
+++ test.py
@@ -20,12 +20,11 @@ model.load_state_dict(checkpoint['model_state_dict'])
 model.eval()
 
 # Load annotation CSV file
-train_csv = pd.read_csv('../input/movie-classifier/Multi_Label_dataset/train.csv')
-genres = train_csv.columns.values[2:]
+test_csv = pd.read_csv('/content/multi-label-classification-pytorch/dataset/data_total3/test/test.csv')
 
 # Prepare the test dataset and dataloader
 test_data = ImageDataset(
-    train_csv, train=False, test=True
+    test_csv, size=(224,224)
 )
 
 test_loader = DataLoader(
@@ -36,23 +35,20 @@ test_loader = DataLoader(
 
 for counter, data in enumerate(test_loader):
     image, target = data['image'].to(device), data['label']
+
     # get all the index positions where value == 1
     target_indices = [i for i in range(len(target[0])) if target[0][i] == 1]
+    
     # get the predictions by passing the image through the model
     outputs = model(image)
     outputs = torch.sigmoid(outputs)
     outputs = outputs.detach().cpu()
-    sorted_indices = np.argsort(outputs[0])
-    best = sorted_indices[-3:]
-    string_predicted = ''
-    string_actual = ''
-    for i in range(len(best)):
-        string_predicted += f"{genres[best[i]]}    "
-    for i in range(len(target_indices)):
-        string_actual += f"{genres[target_indices[i]]}    "
-    image = image.squeeze(0)
-    image = image.detach().cpu().numpy()
-    image = np.transpose(image, (1, 2, 0))
+    #sorted_indices = np.argsort(outputs[0])
+
+    print(outputs)
+    # image = image.squeeze(0)
+    # image = image.detach().cpu().numpy()
+    # image = np.transpose(image, (1, 2, 0))
     # plt.imshow(image)
     # plt.axis('off')
     # plt.title(f"PREDICTED: {string_predicted}\nACTUAL: {string_actual}")
