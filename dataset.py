6f7848865f74ae5b788798015f260a4ad28674fa
HOÀNG NGỌC BÁ THI <115174809+thihoangngocba@users.noreply.github.com>
2023-08-18 14:46:58 +0700

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -4,59 +4,34 @@ import numpy as np
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset
 
-class ImageDataset(Dataset):
-    def __init__(self, csv, train, test):
+class MultiAttribDataset(Dataset):
+    def __init__(self, csv, size=(224,224)):
         self.csv = csv
-        self.train = train
-        self.test = test
-        #self.all_image_names = self.csv[:]['Id']
-        #self.all_labels = np.array(self.csv.drop(['Id', 'Genre'], axis=1))
-        self.train_ratio = int(0.85 * len(self.csv))
-        self.valid_ratio = len(self.csv) - self.train_ratio
+        self.image_names = self.csv[:]['Filenames']
+        self.labels = np.array(self.csv.drop(['Filenames'], axis=1))
 
         # Set the training data images and labels
-        if self.train == True:
-            print(f"Number of training images: {self.train_ratio}")
-            self.image_names = list(self.all_image_names[:self.train_ratio])
-            self.labels = list(self.all_labels[:self.train_ratio])
-            # define the training transforms
-            self.transform = transforms.Compose([
-                transforms.ToPILImage(),
-                transforms.Resize((400, 400)),
-                transforms.RandomHorizontalFlip(p=0.5),
-                transforms.RandomRotation(degrees=45),
-                transforms.ToTensor(),
-            ])
+        print(f"Number of images: {self.image_names}")
 
-        # Set the validation data images and labels
-        elif self.train == False and self.test == False:
-            print(f"Number of validation images: {self.valid_ratio}")
-            self.image_names = list(self.all_image_names[-self.valid_ratio:-10])
-            self.labels = list(self.all_labels[-self.valid_ratio:-10])
-            # define the validation transforms
-            self.transform = transforms.Compose([
-                transforms.ToPILImage(),
-                transforms.Resize((400, 400)),
-                transforms.ToTensor(),
-            ])
-
-        elif self.test == True and self.train == False:
-            self.image_names = list(self.all_image_names[-10:])
-            self.labels = list(self.all_labels[-10:])
-             # define the test transforms
-            self.transform = transforms.Compose([
-                transforms.ToPILImage(),
-                transforms.ToTensor(),
-            ])
+        # Define the training transforms
+        self.transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Resize(size),
+            #transforms.RandomHorizontalFlip(p=0.5),
+            #transforms.RandomRotation(degrees=45),
+            transforms.ToTensor(),
+        ])
             
     def __len__(self):
         return len(self.image_names)
     
     def __getitem__(self, index):
-        image = cv2.imread(f"../dataset/images/{self.image_names[index]}.jpg")
-        # convert the image from BGR to RGB color format
+        image = cv2.imread(f"/content/multi-label-classification-pytorch/dataset/images/{self.image_names[index]}")
+        
+        # Convert the image from BGR to RGB color format
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        # apply image transforms
+        
+        # Apply image transforms
         image = self.transform(image)
         targets = self.labels[index]
         
